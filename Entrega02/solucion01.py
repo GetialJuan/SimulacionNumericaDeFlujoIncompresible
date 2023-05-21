b0cfb9180f00bf863e5cdaed7cd5cf053927a1ab
GetialJuan <getial.juan@correounivalle.edu.co>
2023-05-21 13:00:26 -0500

    Add basic structure

---
+++ Entrega02/solucion01.py
@@ -0,0 +1,2 @@
+# Simulación del fluido empleando sistemas de ecuaciones lineales:
+# Empleando los métodos de Gauss-Seidel y Gradiente Conjugado
---
+++ Entrega02/solucion02.py
@@ -0,0 +1,3 @@
+# Simulación del fluido empleando sistemas de ecuaciones NO lineales:
+# Empleando el método de Newton-Raphson para solucionar los sistemas de ecuaciones
+# No lineales obtenidos
